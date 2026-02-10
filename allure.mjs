7b7efbcc31c0e3c40b3af2ff87065ebb866fb65e
NikitaVorobushek <nikvorobushek@gmail.com>
2026-02-10 23:13:19 +0300

    fix configs

--- allure.mjs
+++ allure.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "allure";
 
 export default defineConfig({
-  name: "Allure Report Example",
+  name: "Allure Report",
   output: "./allure-report",
+  historyPath: './history.jsonl',
   plugins: {
     awesome: {
       options: {
