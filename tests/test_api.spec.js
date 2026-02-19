d375b6bc75a59d0fdcdebd165bf958cc3fcbe7d5
Nikita Vorobyev <62428570+NikitaVorobushek@users.noreply.github.com>
2026-02-19 10:48:47 +0300

    Update test_api.spec.js
    fix progressdata

--- tests/test_api.spec.js
+++ tests/test_api.spec.js
@@ -4,13 +4,12 @@ import { TodoBuilder } from '../src/helpers/builders/index';
 import 'dotenv/config';
 
 test.describe('API challenges', () => {
-  let progressData;
 
   test.afterAll(async ({ request }) => {
     await request.dispose();
   });
     
-  test("02 Get the list of challenges @API_pozitive", async ({ api, challengerToken }) => {
+  test("02 Get the list of challenges @API_positive", async ({ api, challengerToken }) => {
     
     let response = await api.challengesService.get(challengerToken);
     let body = await response.json();
@@ -21,7 +20,7 @@ test.describe('API challenges', () => {
     expect(body.challenges.length).toBeGreaterThan(0);
   });
 
-  test("03 Get todos 200oK @API_pozitive", async ({ api, challengerToken }) => {
+  test("03 Get todos 200 OK @API_positive", async ({ api, challengerToken }) => {
     let response = await api.todosService.get(challengerToken);
     let body = await response.json();
       
@@ -36,7 +35,7 @@ test.describe('API challenges', () => {
     expect(response.status()).toBe(404);
   });
 
-  test("05 Get id of todos 200oK  @API_pozitive", async ({ api, challengerToken }) => {
+  test("05 Get id of todos 200 OK  @API_positive", async ({ api, challengerToken }) => {
     let response = await api.todosService.getById(challengerToken);
     let body = await response.json();
             
@@ -53,7 +52,7 @@ test.describe('API challenges', () => {
     expect(body.errorMessages).toBeTruthy();
   });
 
-  test("07 Get todos with filter DONE 200oK @API_pozitive", async ({ api, challengerToken }) => {
+  test("07 Get todos with filter DONE 200 OK @API_positive", async ({ api, challengerToken }) => {
     const todoBuilder = new TodoBuilder()
       .addTitle()
       .addStatus(true)
@@ -61,21 +60,21 @@ test.describe('API challenges', () => {
       .generate();
     let response = await api.todosService.post(challengerToken, todoBuilder);
 
-    response = await api.todosService.getByfilter(challengerToken, 'doneStatus=true');
+    response = await api.todosService.getByFilter(challengerToken, 'doneStatus=true');
     let body = await response.json();
             
     expect(response.status()).toBe(200);
     expect(body.todos[0]).toEqual(expect.objectContaining({ "doneStatus": true  }))
   });
 
-  test("08 Head todos 200oK  @API_pozitive", async ({ api, challengerToken }) => {
+  test("08 Head todos 200 OK  @API_positive", async ({ api, challengerToken }) => {
     let response = await api.todosService.head(challengerToken);
             
     expect(response.status()).toBe(200);
     expect(response.headers()).toEqual(expect.objectContaining({ "x-challenger": challengerToken }));
   });
 
-  test("09 Post/create todo 201oK @API_pozitive", async ({ api, challengerToken }) => {
+  test("09 Post/create todo 201 OK @API_positive", async ({ api, challengerToken }) => {
     const todoBuilder = new TodoBuilder()
       .addTitle()
       .addStatus(true)
@@ -116,17 +115,17 @@ test.describe('API challenges', () => {
     expect(body.errorMessages).toBeTruthy();
   });
 
-  test("34 Get challenger guid (existing X-CHALLENGER) @API_pozitive", async ({ api, challengerToken }) => {
+  test("34 Get challenger guid (existing X-CHALLENGER) @API_positive", async ({ api, challengerToken }) => {
     let response = await api.challengerService.get(challengerToken, challengerToken);
-    progressData = await response.json();
+    let progressData = await response.json();
         
     expect(response.status()).toBe(200);
     expect(progressData).toHaveProperty('xAuthToken', 'xChallenger', 'secretNote', 'challengeStatus')
   });
 
-  test("35 Put challenger guid RESTORE to memory@API_pozitive", async ({ api, challengerToken }) => {
+  test("35 Put challenger guid RESTORE to memory @API_positive", async ({ api, challengerToken }) => {
     let response = await api.challengerService.get(challengerToken, challengerToken);
-    progressData = await response.json();
+    let progressData = await response.json();
     response = await api.challengerService.put(challengerToken, challengerToken, progressData);
                 
     expect(response.headers()).toEqual(expect.objectContaining({ "x-challenger": challengerToken }));
